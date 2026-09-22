3b8d028aa43bf916e7e27e72335d7b453f61e4a5
Art <63970728+aistmusic@users.noreply.github.com>
2026-09-22 23:41:42 +0200

    iOS fullscreen: hide panel instead of Fullscreen API

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "led-board-v3";
+const CACHE = "led-board-v4";
 const ASSETS = ["./", "index.html", "app.js", "manifest.json", "icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));

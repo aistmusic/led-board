5c95899211bca51cba268ce7816b12afeff868ab
Art <63970728+aistmusic@users.noreply.github.com>
2026-09-24 04:29:23 +0200

    Size actually scales letters; single round color picker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "led-board-v8";
+const CACHE = "led-board-v9";
 const ASSETS = ["./", "index.html", "app.js", "manifest.json", "icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
